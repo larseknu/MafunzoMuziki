1b7175427acf3b294cff5962b86e20067f0f386c
Lars Emil Knudsen <larsemil87@gmail.com>
2017-11-30 23:25:29 +0100

    Added play, pause, skip, padded number

--- app/src/main/java/no/hiof/larseknu/mafonzomuziki/MainActivity.java
+++ app/src/main/java/no/hiof/larseknu/mafonzomuziki/MainActivity.java
@@ -1,11 +1,14 @@
 package no.hiof.larseknu.mafonzomuziki;
 
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.graphics.Color;
 import android.os.CountDownTimer;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.spotify.sdk.android.authentication.AuthenticationClient;
@@ -20,10 +23,10 @@ import com.spotify.sdk.android.player.PlayerEvent;
 import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.SpotifyPlayer;
 
-import java.util.Arrays;
-import java.util.List;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity implements SpotifyPlayer.NotificationCallback, ConnectionStateCallback {
+    private static final String TAG = "Mafonzo";
 
     // region top secret
     private static final String CLIENT_ID = "0f835bef4ef44912ac05055c3d099b1b";
@@ -36,13 +39,17 @@ public class MainActivity extends AppCompatActivity implements SpotifyPlayer.Not
     private TextView artistTextView;
     private TextView statusTextView;
     private TextView timeTextView;
+    private ImageButton playPauseButton;
+    private ConstraintLayout container;
 
     private Player player;
-    CountDownTimer countDownTimer;
+    private CountDownTimer countDownTimer;
+    private boolean paused = false;
 
     long remainingTime = 0;
     private static long PAUSE_TIME = 10000;
-    private static long PLAY_TIME = 50000;
+    private static long PLAY_TIME = 10000;
+    private static DecimalFormat timerFormat = new DecimalFormat("00.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,6 +68,8 @@ public class MainActivity extends AppCompatActivity implements SpotifyPlayer.Not
         artistTextView = findViewById(R.id.artistTextView);
         statusTextView = findViewById(R.id.statusTextView);
         timeTextView = findViewById(R.id.timeTextView);
+        playPauseButton = findViewById(R.id.playPauseButton);
+        container = findViewById(R.id.container);
     }
 
     @Override
@@ -91,19 +100,18 @@ public class MainActivity extends AppCompatActivity implements SpotifyPlayer.Not
     }
 
     private void startPlayCountdown(long playTime) {
-        countDownTimer = new CountDownTimer(playTime, 1000) {
+        countDownTimer = new CountDownTimer(playTime, 10) {
 
             public void onTick(long millisUntilFinished) {
-                Log.d("MainActivity", "Play seconds remaining " + millisUntilFinished / 1000);
+                //Log.d("MainActivity", "Play seconds remaining " + millisUntilFinished / 1000);
                 remainingTime = millisUntilFinished;
-                timeTextView.setText(remainingTime / 1000 + "");
+                timeTextView.setText(timerFormat.format(remainingTime / 1000.0));
             }
 
             public void onFinish() {
-                //mTextField.setText("done!");
                 Log.d("MainActivity", "Play done, pausing music for: " + PAUSE_TIME / 1000);
                 player.pause(null);
-                statusTextView.setText("PAUSED");
+                statusTextView.setText(R.string.paused);
                 startPauseCountDown(PAUSE_TIME);
             }
         };
@@ -111,19 +119,18 @@ public class MainActivity extends AppCompatActivity implements SpotifyPlayer.Not
     }
 
     private void startPauseCountDown(long pauseTime) {
-        countDownTimer = new CountDownTimer(pauseTime, 1000) {
+        countDownTimer = new CountDownTimer(pauseTime, 10) {
 
             public void onTick(long millisUntilFinished) {
-                Log.d("MainActivity", "Pause seconds remaining " + millisUntilFinished / 1000);
+                //Log.d("MainActivity", "Pause seconds remaining " + millisUntilFinished / 1000);
                 remainingTime = millisUntilFinished;
-                timeTextView.setText(remainingTime / 1000 + "");
+                timeTextView.setText(timerFormat.format(remainingTime / 1000.0));
             }
 
             public void onFinish() {
-                //mTextField.setText("done!");
                 Log.d("MainActivity", "Pause done, playing music for "+ PLAY_TIME / 1000);
                 player.skipToNext(null);
-                statusTextView.setText("PLAYING");
+                statusTextView.setText(R.string.playing);
                 startPlayCountdown(PLAY_TIME);
             }
         };
@@ -144,6 +151,8 @@ public class MainActivity extends AppCompatActivity implements SpotifyPlayer.Not
             case kSpPlaybackNotifyPlay:
                 UpdateUIWithCurrentSong(player.getMetadata());
             case kSpPlaybackNotifyNext:
+                UpdateUIWithCurrentSong(player.getMetadata());
+            case kSpPlaybackNotifyPrev:
                 UpdateUIWithCurrentSong(player.getMetadata());
             default:
                 break;
@@ -170,9 +179,9 @@ public class MainActivity extends AppCompatActivity implements SpotifyPlayer.Not
     public void onLoggedIn() {
         Log.d("MainActivity", "User logged in");
 
-        player.playUri(null, "spotify:user:1147093756:playlist:29ReuwbKFCHRxueAqlsQzs", 0, 0);
+        player.playUri(null, "spotify:user:johanbrook:playlist:2mtlhuFVOFMn6Ho3JmrLc2", 0, 0);
         startPlayCountdown(PLAY_TIME);
-        statusTextView.setText("PLAYING");
+        statusTextView.setText(R.string.playing);
     }
 
     @Override
@@ -194,4 +203,47 @@ public class MainActivity extends AppCompatActivity implements SpotifyPlayer.Not
     public void onConnectionMessage(String message) {
         Log.d("MainActivity", "Received connection message: " + message);
     }
+
+    public void playButtonClicked(View view) {
+        if (player.getPlaybackState().isPlaying) {
+            player.pause(defaultOperationCallback);
+            countDownTimer.cancel();
+            playPauseButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_play_circle_outline));
+            statusTextView.setText(R.string.paused);
+            paused = true;
+            container.setBackgroundColor(Color.RED);
+        }
+        else {
+            playPauseButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_pause_circle_outline));
+            player.resume(defaultOperationCallback);
+            startPlayCountdown(remainingTime);
+            statusTextView.setText(R.string.playing);
+            paused = false;
+            container.setBackgroundColor(Color.GREEN);
+        }
+    }
+
+    public void skipNextButtonClicked(View view) {
+        player.skipToNext(defaultOperationCallback);
+        if (paused)
+            player.pause(defaultOperationCallback);
+    }
+
+    public void skipPreviousButtonClicked(View view) {
+        player.skipToPrevious(defaultOperationCallback);
+        if (paused)
+            player.pause(defaultOperationCallback);
+    }
+
+    Player.OperationCallback defaultOperationCallback = new Player.OperationCallback() {
+        @Override
+        public void onSuccess() {
+
+        }
+
+        @Override
+        public void onError(Error error) {
+            Log.d(TAG, error.toString());
+        }
+    };
 }
